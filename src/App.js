d063e5031f647469c6c8bb19af447527fcf471cf
Zehra Khan <khan77040307@gmail.com>
2022-11-09 13:36:53 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./screens/Home";
+import Playground from "./screens/Playground";
+import Error404 from "./screens/Error404";
+
 function App() {
   return (
-    <div className="App">
-      code-deck
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/playground" element={<Playground />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
   );
 }
 
