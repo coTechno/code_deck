317ac499bdfe4495c397567e9780f50c57c87a95
Zehra Khan <khan77040307@gmail.com>
2022-11-09 22:22:10 +0530

    leftComponent

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
 import Playground from "./screens/Playground";
 import Error404 from "./screens/Error404";
+import { GlobalStyle } from "./style/global";
 
 function App() {
   return (
+    <>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/playground" element={<Playground />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
+    </>
   );
 }
 
---
+++ src/screens/Home/RightComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RightComponent = () => {
+  return (
+    <div>RightComponent</div>
+  )
+}
+
+export default RightComponent
